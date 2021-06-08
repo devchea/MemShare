72da7b6f810521afc1991e12babfbc1104a71a02
devchea <rchea817@gmail.com>
2021-06-08 16:12:53 -0700

    "updates"

--- server/index.js
+++ server/index.js
@@ -1,5 +1,21 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import bodyParser from 'body-parser'
-import monboose from 'mongoose'
+import mongoose from 'mongoose'
 import cors from 'cors'
 
+const config = dotenv.config().parsed
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.8dak5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
